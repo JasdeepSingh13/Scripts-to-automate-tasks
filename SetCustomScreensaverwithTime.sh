851eb774e9b903458b264c60646af9f1bf6dc1d1
Jasdeep Singh <jasdeep_singh13@yahoo.co.in>
2023-05-03 20:50:09 +0530

    Add files via upload

---
+++ SetCustomScreensaverwithTime.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# VARIABLES
+screenSaverModuleName="Helios"
+screenSaverFileName="Helios.saver"
+screenSaverPath="/Library/Screen Savers"
+
+
+# DO NOT EDIT BELOW THIS LINE
+user=$(last | grep "logged in" | grep "console" | awk '{print $1}');
+huuid=$(system_profiler SPHardwareDataType | awk '/Hardware UUID/ {print $3}')
+
+/usr/libexec/PlistBuddy -c "Set moduleDict:moduleName $screenSaverModuleName" /Users/$user/Library/Preferences/ByHost/com.apple.screensaver.$huuid.plist
+/usr/libexec/PlistBuddy -c "Set moduleDict:path $screenSaverPath/$screenSaverFileName" /Users/$user/Library/Preferences/ByHost/com.apple.screensaver.$huuid.plist
+
+setName=$(/usr/libexec/PlistBuddy -c "Print moduleDict:moduleName" /Users/$user/Library/Preferences/ByHost/com.apple.screensaver.$huuid.plist)
+setFile=$(/usr/libexec/PlistBuddy -c "Print moduleDict:path" /Users/$user/Library/Preferences/ByHost/com.apple.screensaver.$huuid.plist)
+
+
+defaults -currentHost write com.apple.screensaver idleTime -int 60
+
+
+killall cfprefsd
