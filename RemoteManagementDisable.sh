731d0b02bd0d1eecd6c42a999b3aca8d5d676dbf
Jasdeep Singh <jasdeep_singh13@yahoo.co.in>
2023-05-19 15:39:59 +0530

    RemoteManagementDisable.sh

--- RemoteManagementDisable.sh
+++ RemoteManagementDisable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+######################################################## 
+
+# Author: Jasdeep Singh (jasdeep_singh@yahoo.co.in)
+
+######################################################## 
+
 #Disable remote management ARD
 
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
