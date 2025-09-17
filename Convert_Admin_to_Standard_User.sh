fac8d4aeedbb20a8c29ab48d7375d7912105e265
Jasdeep Singh <jasdeep_singh13@yahoo.co.in>
2025-09-17 19:53:12 +0530

    Add files via upload

---
+++ Convert_Admin_to_Standard_User.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+#
+#     Convert_Admin_to_Standard_User.sh
+#     Remove admin privileges from logged in user
+#
+
+
+
+####################
+
+
+
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
+
+
+
+####################
+
+
+
+# Remove admin privileges from $loggedInUser.
+if /usr/bin/dscl . -read "/groups/admin" GroupMembership | /usr/bin/grep -q "$loggedInUser"; then
+  /usr/sbin/dseditgroup -o edit -d "$loggedInUser" admin
+  echo "Removed $loggedInUser admin privileges."
+else
+  echo "$loggedInUser is already a standard user, no action required."
+fi
+
+
+
+exit 0
